51ad1f59182fe091c825ae2c11cccb7c57653df5
subzerodel <58028949+subzerodel@users.noreply.github.com>
2021-01-23 17:45:09 +0600

    Add files via upload

---
+++ ChatProject/Repositories/IRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿using ChatProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatProject.Repositories
+{
+    public interface IRoomRepository
+    {
+        public void CreateRoom(Room room);
+        public Room GetRoom(int roomId);
+    }
+}
---
+++ ChatProject/Repositories/Implementation/MessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using ChatProject.Data;
+using System.Threading.Tasks;
+
+namespace ChatProject.Repositories.Implementation
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public MessageRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<int> NewMessage(Models.Message message)
+        {
+            var newMsg = _dataContext.Messages.Add(message);
+            await _dataContext.SaveChangesAsync();
+            return newMsg.Entity.Id;
+        }
+    }
+}
